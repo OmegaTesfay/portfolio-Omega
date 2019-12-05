4d35b3a2f6f86fc1db71e93cfbd92231d5fb9670
OmegaTesfay <omihb16@gmail.com>
2019-12-04 16:44:43 -0800

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 // import { Material } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HomeComponent } from './home/home.component';
+
 
 import {ResumeComponent} from '../resume/resume.component';
 import { BioComponent } from '../bio/bio.component';
@@ -14,13 +14,13 @@ import { SocialmediaComponent } from '../socialmedia/socialmedia.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
-    { path: '', component: HomeComponent },
+    
     { path: 'resume', component: ResumeComponent },
     { path: 'bio', component: BioComponent },
     { path: 'socialmedia', component: SocialmediaComponent }
 
   ]) ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, ResumeComponent, BioComponent, SocialmediaComponent ],
+  declarations: [ AppComponent, HelloComponent,  ResumeComponent, BioComponent, SocialmediaComponent ],
   providers: [],
   bootstrap:    [ AppComponent ]
 })
