51948f6bb2aa49cc0bb97e1131e6f30f0c1a66a0
OmegaTesfay <omihb16@gmail.com>
2019-12-04 16:54:13 -0800

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-// import { Material } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
-
 import {ResumeComponent} from '../resume/resume.component';
 import { BioComponent } from '../bio/bio.component';
 import { SocialmediaComponent } from '../socialmedia/socialmedia.component';
